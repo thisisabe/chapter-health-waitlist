8114b97852bb05dec0761966714cb73993d37a8c
thisisabe <abe@thinkermaker.com.au>
2025-08-22 01:42:01 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'health-core': '#18051E',
+        'light-balance': '#E8CDFF',
+        'restorative-yellow': '#FBF999',
+        'peak-performance': '#FF482C',
+      },
+      fontFamily: {
+        heading: ["var(--font-heading)", "sans-serif"],
+        body: ["var(--font-body)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
